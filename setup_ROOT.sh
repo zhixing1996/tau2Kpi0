016d3b59dda3da26dfc9b284cb495116d50fd556
zhixing1996 <jingmaoqiang1996@163.com>
2021-08-31 10:51:17 +0800

    add environment setup tools

---
+++ setup_ROOT.sh
@@ -0,0 +1,2 @@
+source /cvmfs/bes3.ihep.ac.cn/bes3sw/ExternalLib/SLC6/ExternalLib/external/ROOT/6.12.06/config/thisroot.sh
+alias python='python3'
---
+++ setup_gbasf2.sh
@@ -0,0 +1,2 @@
+source /besfs5/groups/psip/psipgroup/user/jingmq/gbasf2/BelleDIRAC/gbasf2/tools/setup
+gb2_proxy_init -g belle
